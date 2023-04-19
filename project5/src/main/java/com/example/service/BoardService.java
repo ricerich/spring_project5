3b92ad6f205f82aca1bf1909df264dbae22835ec
admin <admin@DESKTOP-BSVD8GJ>
2023-04-19 17:33:27 +0900

    스프링 게시판

---
+++ project5/src/main/java/com/example/service/BoardService.java
@@ -0,0 +1,24 @@
+package com.example.service;
+
+import java.util.List;
+
+import com.example.domain.BoardVO;
+
+public interface BoardService {
+
+	public void register(BoardVO board);
+
+	public BoardVO get(Long bno);
+
+	public boolean modify(BoardVO board);
+
+	public boolean remove(Long bno);
+
+	 public List<BoardVO> getList();
+
+//	public List<BoardVO> getList(Criteria cri);
+
+	//Ãß°¡
+//	public int getTotal(Criteria cri);
+
+}
